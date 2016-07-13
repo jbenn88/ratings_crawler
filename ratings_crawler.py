32a9a20e276117936c16c9ef8e2a036b5693eef2
jbenn88 <jamesa.benn@gmail.com>
2016-07-13 08:14:20 -0400

    formatting

--- ratings_crawler.py
+++ ratings_crawler.py
@@ -21,16 +21,3 @@ soup = BeautifulSoup(plain_text, "html.parser")
 for item in soup.findAll('div', {'class' : 'user_review'}):
     print(item.text)
     print()
-"""
-date_today = date.today()
-print()
-print("Headlines for " + calendar.day_name[date_today.weekday()] + " {:%B %d, %Y}".format(date_today) + ":\n")
-
-url = 'https://www.rottentomatoes.com/'
-user_agent = {
-    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
-source_code = requests.get(url, headers=user_agent)
-plain_text = source_code.text
-soup = BeautifulSoup(plain_text, "html.parser")
-
-"""
